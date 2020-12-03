8495c504e8165ed589db3bde3a84ec90f1a39dde
Ivan Mitrovic <ivanmitrovicenemy@gmail.com>
2020-12-03 20:00:34 +0100

    Prvi Commit odradjena troslojna arhitektura

---
+++ Projekat/DataLayer/LeagueRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DataLayer
+{
+    public class LeagueRepository
+    {
+
+        public List<League> GetAllLeagues()
+        {
+
+            public string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LeagueDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            List<League> results = new List<League>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Leagues";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    League s = new League();
+                    s.Id = sqlDataReader.GetInt32(0);
+                    s.Name = sqlDataReader.GetString(1);
+                    s.Couch = sqlDataReader.GetString(2);
+                    s.Points = sqlDataReader.GetInt32(3);
+
+                    results.Add(s);
+                }
+            }
+
+            return results;
+        }
+
+        public int InsertLeague(League s)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Leagues VALUES ('{0}', '{1}', {2})",
+                        s.Name, s.Couch, s.Points);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
+    }
+}
---
+++ Projekat/DataLayer/Models/League.cs
@@ -0,0 +1,57 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Models
+{
+    class League
+    {
+        public class League
+        {
+            private int Id ;
+            private string Name;
+            private int Points ;
+            private string Couch;
+
+            public League(string id, string Name, string Couch, int Points)
+            {
+                Id = Id;
+                Name = name;
+                Couch = couch;
+                Points = points;
+            }
+            
+        
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public string couch
+        {
+            get { return Couch; }
+            set { Couch = value; }
+        }
+        public int points
+        {
+            get { return Points; }
+            set { Points = value; }
+        }
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Couch} {Points}";
+        }
+
+}
+}
+
+    }
+
