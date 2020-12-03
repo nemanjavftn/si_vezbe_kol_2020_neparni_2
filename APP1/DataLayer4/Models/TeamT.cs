1e4aa58b0a44f07b4e5afd24853dee03a20c4120
Marta Mladenovic <marta.m2000@yahoo.com>
2020-12-03 19:36:31 +0100

    Commit 2 DataLayer created

---
+++ APP1/DataLayer4/Models/TeamT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer4.Models
+{
+    public class TeamT
+    {
+        private int Id;
+        private string Name;
+        private string Couch;
+        private int Points;
+
+        public TeamT(int id, string name, string couch, int points)
+        {
+            Id1 = id;
+            Name1 = name;
+            Couch1 = couch;
+            Points1 = points;
+        }
+
+        public int Id1 { get => Id; set => Id = value; }
+        public string Name1 { get => Name; set => Name = value; }
+        public string Couch1 { get => Couch; set => Couch = value; }
+        public int Points1 { get => Points; set => Points = value; }
+        public override string ToString()
+        {
+            return $"{Id}-{Name}-{Couch}-{Points}";
+    }
+    }
+  
+}
---
+++ APP1/DataLayer4/TeamRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataLayer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer4
+{
+    public class TeamRepository
+    {
+
+        public List<TeamT> GetAllTeams()
+        {
+            List<TeamT> teamList = new List<TeamT>();
+            string sqlcon = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LeagueDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(sqlcon))
+            {
+                con.Open();
+                string commandText = "select * from Teams";
+                SqlCommand com = new SqlCommand(commandText, con);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    TeamT t = new TeamT(dr.GetInt32(0), dr.GetString(1), dr.GetString(2), dr.GetInt32(3));
+                    teamList.Add(t);
+                }
+            }
+            return teamList;
+        }
+        public int InsertTeam(TeamT t)
+        {
+            int result;
+            string sqlcon = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LeagueDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(sqlcon))
+            {
+                con.Open();
+                string commandText = "insert into Teams(Name,Couch,Points) values(@nam, @cou, @poi)";
+                SqlCommand com = new SqlCommand(commandText, con);
+                com.Parameters.AddWithValue("@nam", t.Name1);
+                com.Parameters.AddWithValue("@cou", t.Couch1);
+                com.Parameters.AddWithValue("@averagem", t.Points1);
+                result = com.ExecuteNonQuery();
+            }
+            return result;
+        }
+
+
+    }
+}
