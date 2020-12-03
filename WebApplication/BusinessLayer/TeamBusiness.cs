b2378151dbc0c26e95fc60346e954f0e6c5f7e8f
Darko Djapovic <redda999@gmail.com>
2020-12-03 20:13:56 +0100

    Zavrsni komitt

---
+++ WebApplication/BusinessLayer/TeamBusiness.cs
@@ -0,0 +1,42 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class TeamBusiness
+    {
+      public TeamRepository tr;
+            public TeamBusiness()
+            {
+
+                this.tr = new TeamRepository();
+
+            }
+            public List<Team> GetallTeams()
+            {
+                return this.tr.GetAllTeams();
+            }
+            public bool insertTeam(Team t)
+            {
+                int result = this.tr.insertTeam(t);
+                if (result != 0)
+
+                    return true;
+                return false;
+            
+            }
+        public List<Team> SortTeams(Team t)
+        {
+            TeamBusiness tb = new TeamBusiness();
+            List<Team> Team = tb.GetallTeams();
+            Team.OrderBy(x => x.Points1).ToList();
+            return Team;
+        }
+       
+        }
+}
---
+++ WebApplication/WebApplication/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            TeamBusiness tb = new TeamBusiness();
+            List<Team> tl = tb.GetallTeams();
+            foreach (Team t in tl)
+            {
+                ListBox1.Items.Add(t.ToString());
+            }
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TeamBusiness ab = new TeamBusiness();
+            Team t = new Team (1, TextBox1.Text,  TextBox2.Text, Convert.ToInt32(TextBox3.Text));
+            bool result = ab.insertTeam(t);
+            ListBox1.Items.Clear();
+            Page_Load(null, null);
+        }
+    }
+}
